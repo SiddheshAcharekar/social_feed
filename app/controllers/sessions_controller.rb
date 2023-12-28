5670ec6b784b2fb87c66218a7a6c27f0810e42a8
SiddheshAcharekar <siddheshsacharekar@gmail.com>
2023-12-28 21:26:58 +0530

    Add sessions for remember me

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       reset_session
-      log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember user
+      log_in user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email or password'
@@ -15,5 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def delete
+    log_out if logged_in?
+    redirect_to root_url, status: :see_other
   end
 end
